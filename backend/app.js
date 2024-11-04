f8664385fbe05f072bb8c1917e15302fc0f7928a
madhulagg <f20221236@goa.bits-pilani.ac.in>
2024-11-04 23:23:25 +0530

    “update”

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT
+
+const server = () => {
+    console.log("PORT: ", PORT)
+}
+
+
+server()
